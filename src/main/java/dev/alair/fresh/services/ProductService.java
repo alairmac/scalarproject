3c361df96b2d820c88d1980eac3be15a447644ce
alairmac <42205093+alairmac@users.noreply.github.com>
2024-04-29 21:25:59 +0530

    Update ProductService.java

--- src/main/java/dev/alair/fresh/services/ProductService.java
+++ src/main/java/dev/alair/fresh/services/ProductService.java
@@ -11,7 +11,8 @@ public interface ProductService {
     public List<Product> getAllProduct();
     public List<Category> getAllCategories();
     public Product updateProduct(Long id, String title, String description, String image,String category,double price);
-    public Product deleteProduct(Long id);
+//    public Product deleteProduct(Long id);
+    public Boolean deleteProduct(Long id);
     public List<Product> getCategoryProduct(String category);
     public Product createProduct(String title, String description, String image,String category,double price);
     public Category createCategory(String category);
